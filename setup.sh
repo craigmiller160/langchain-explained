a40b23b6fffad466827305ac0bdb1a5acdda7ae8
Craig Miller <craigmiller160@gmail.com>
2024-01-14 11:15:37 -0500

    Tweaked setup script

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ python3 -m pip install chromadb
 #docker compose up -d
 #docker exec -it ollama ollama run llama2
 
-echo "Everything is setup, but you need to run ollama/llama2 on your server"
+echo ""
+echo "*************************************************************************"
+echo "* Everything is setup, but you need to run ollama/llama2 on your server *"
+echo "*************************************************************************"
