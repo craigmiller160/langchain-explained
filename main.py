971806c6ae6620d968f337cd397e39cf4e963f82
Craig Miller <craigmiller160@gmail.com>
2024-01-14 09:02:16 -0500

    Some progress

---
+++ main.py
@@ -0,0 +1,4 @@
+from langchain_community.llms import Ollama
+
+ollama = Ollama(base_url='http://localhost:11434', model='llama2')
+print(ollama('Why is the sky blue?'))
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 -m pip install -r requirements.txt
+python3 -m pip install langchain
+python3 -m pip install langchain-community
