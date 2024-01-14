995cd488cfbab041a824d968b088ed69c83f7efc
Craig Miller <craigmiller160@gmail.com>
2024-01-14 10:45:55 -0500

    Progress

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 from langchain_community.llms import Ollama
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OllamaEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
 
-ollama = Ollama(base_url='http://localhost:11434', model='llama2')
-print(ollama('Why is the sky blue?'))
+# TODO maybe need: pip install GPT4All chromadb
+
+OLLAMA_HOST = 'http://localhost:11434'
+OLLAMA_MODEL = 'llama2'
+
+ollama = Ollama(base_url=OLLAMA_HOST, model=OLLAMA_MODEL)
+oembed = OllamaEmbeddings(base_url=OLLAMA_HOST, model=OLLAMA_MODEL)
+
+
+def simple_prompt():
+    print(ollama('Why is the sky blue?'))
+
+
+def document_prompt():
+    loader = WebBaseLoader("https://www.gutenberg.org/files/1727/1727-h/1727-h.htm")
+    data = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
+
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=oembed)
+
+    question = "Who is Neleus and who is in Neleus' family?"
+    docs = vectorstore.similarity_search(question)
+
+    qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
+    qachain({"query": question})
+
+
+simple_prompt()
